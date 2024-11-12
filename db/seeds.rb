b9735660fb2fe45fff656d0aa087cf9ddf60ef8e
kristen harnois <kristen.harnois@gmail.com>
2024-11-12 16:50:59 +0100

    exercice ok, manque refacto & un peu de front

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  a = Article.new(title: Faker::Game.title, content: Faker::Movies::HarryPotter.spell)
+  a.save
+end
